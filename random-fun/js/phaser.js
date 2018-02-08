e69dc12be2914e45a4bd6986a2eeaa6236630730
segfaultdoctor <sherwanizanyar@gmail.com>
2018-02-08 14:33:56 -0500

    fixed error

--- random-fun/js/phaser.js
+++ random-fun/js/phaser.js
@@ -31023,7 +31023,7 @@ Phaser.Stage = function (game) {
     * @private
     */
    // this._bgColor = { r: 0, g: 0, b: 0, a: 0, color: 0, rgba: '#000000' };
-	this._bgColor = { r: B, g: B, b: B, a: B, color: B, rgba: '#BBBBBB' };
+	this._bgColor = { r: 0, g: 0, b: 0, a: 0, color: 0, rgba: '#BBBBBB' };
 
     if (!this.game.transparent)
     {
