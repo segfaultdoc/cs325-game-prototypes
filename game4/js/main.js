4a78f11d721efb573cbf573f251595e011e08aa2
segfaultdoctor <sherwanizanyar@gmail.com>
2018-03-01 13:06:39 -0500

    stuff

--- game4/js/main.js
+++ game4/js/main.js
@@ -3,15 +3,37 @@
 window.onload = function() {
   const SPEED = 200;
   const GRAVITY = 900;
-     
+  const JET = 420; 
   var ocean;
   var player;
   
   var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
   
   var state = {
+                
+                jet: function(){
+                  if(!this.gameStarted){
+                    this.start();
+                  }
+                  if(!this.gameOver){
+
+                    this.player.body.velocity.y = -JET;
+
+                  } else if(this.time.now > this.timeOver + 400){
+                      this.reset();
+                  }
+    
+                },
+                
+                start: function(){
+                  this.player.body.allowGravity = true;
+                  this.scoreText.setText("SCORE\n"+this.score);
+                  this.gameStarted = true;
+
+                },
+                
                 preload: function(){
-                  var ocean = game.load.image('background', 'assets/ocean.png');
+                  ocean = game.load.image('background', 'assets/ocean.png');
                   //ocean.scale.x = .5;
                   this.load.spritesheet('player', 'assets/polarBear.png', 200, 300);
                   
@@ -21,11 +43,9 @@ window.onload = function() {
                   this.physics.startSystem(Phaser.Physics.ARCADE);
                   this.physics.arcade.gravity.y = GRAVITY;
                   this.background = this.add.tileSprite(0,0,this.world.width, this.world.height, 'background');
-                  //this.background = this.add.sprite(game.world.centerX, game.world.centerY, 'background');
-                  //this.background.scale.setTo(.5,.3);
-                  //this.background.anchor.setTo(.5,.5);
-                  
+                 
                   this.player = this.add.sprite(0,0, 'player');
+                  
                   this.player.animations.add('fly', [0,1,2], 10, true);
                   this.physics.arcade.enableBody(this.player);
                   this.player.body.collideWorldBounds = true;
@@ -41,14 +61,32 @@ window.onload = function() {
 
                         }
                   );
-                    
+                  this.scoreText.anchor.setTo(.5,.5);
+                  this.input.onDown.add(this.jet, this);
+
                   
                   this.reset();
                 },
                 update: function(){
                   if(this.gameStarted){
+                    if(this.player.body.velocity.y > -20){
+                      this.player.frame = 3;
+                    }
+                  } else { 
+                   
+                    this.player.animations.play("fly");
+                  }
+                  if(!this.gameOver){
+                    if(this.player.body.bottom >= this.world.bounds.bottom){
+                        this.setGameOver();
+                    }
 
-                  } else { this.player.y = this.world.centerY + (8*Math.cos(this.time.now/200))}
+                  }
+
+                  else{
+                       this.player.y = this.world.centerY + (8*Math.cos(this.time.now/200));
+                  }
+                  
                 },
                 reset: function(){
                   this.gameStarted =false;
@@ -59,6 +97,11 @@ window.onload = function() {
                   this.player.animations.play('fly');
                   this.background.autoScroll(-SPEED*.8, 0);
                   this.scoreText.setText("Touch To\nStart Game");
+                },
+                setGameOver: function(){
+                  this.gameOver = true;
+                  this.scoreText.setText("FINAL SCORE\n" + this.score+"\n\nTOUCH TO\n TRY AGAIN");;
+                  this.timeOver = this.time.now;
                 }
   }
   
