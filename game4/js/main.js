eb5d680ca9a7b432bd699bdae32a8913c03257e6
segfaultdoctor <sherwanizanyar@gmail.com>
2018-03-01 14:30:18 -0500

    stuff

--- game4/js/main.js
+++ game4/js/main.js
@@ -3,7 +3,9 @@
 window.onload = function() {
   const SPEED = 200;
   const GRAVITY = 900;
-  const JET = 420; 
+  const JET = 420;
+  const OPENING = 200;
+  const SPAWN_RATE = 1.25;
   var ocean;
   var player;
   
@@ -29,11 +31,14 @@ window.onload = function() {
                   this.player.body.allowGravity = true;
                   this.scoreText.setText("SCORE\n"+this.score);
                   this.gameStarted = true;
+                  this.wallTimer = this.game.time.events.loop(Phaser.Timer.SECOND*SPAWN_RATE, this.spawnWalls, this);
+                  this.wallTimer.timer.start();
 
                 },
                 
                 preload: function(){
                   ocean = game.load.image('background', 'assets/ocean.png');
+                  game.load.image('wall', 'assets/walls.png');
                   //ocean.scale.x = .5;
                   this.load.spritesheet('player', 'assets/polarBear.png', 200, 300);
                   
@@ -43,7 +48,7 @@ window.onload = function() {
                   this.physics.startSystem(Phaser.Physics.ARCADE);
                   this.physics.arcade.gravity.y = GRAVITY;
                   this.background = this.add.tileSprite(0,0,this.world.width, this.world.height, 'background');
-                 
+                  this.walls = this.add.group();
                   this.player = this.add.sprite(0,0, 'player');
                   
                   this.player.animations.add('fly', [0,1,2], 10, true);
@@ -63,7 +68,7 @@ window.onload = function() {
                   );
                   this.scoreText.anchor.setTo(.5,.5);
                   this.input.onDown.add(this.jet, this);
-
+                  //this.spawnWall(300);
                   
                   this.reset();
                 },
@@ -72,15 +77,17 @@ window.onload = function() {
                     if(this.player.body.velocity.y > -20){
                       this.player.frame = 3;
                     }
-                  } else { 
+                   else { 
                    
                     this.player.animations.play("fly");
-                  }
+                    }
+                  
                   if(!this.gameOver){
                     if(this.player.body.bottom >= this.world.bounds.bottom){
                         this.setGameOver();
                     }
 
+                  }
                   }
 
                   else{
@@ -100,8 +107,32 @@ window.onload = function() {
                 },
                 setGameOver: function(){
                   this.gameOver = true;
-                  this.scoreText.setText("FINAL SCORE\n" + this.score+"\n\nTOUCH TO\n TRY AGAIN");;
+                  this.scoreText.setText("FINAL SCORE\n" + this.score+"\n\nTOUCH TO\n TRY AGAIN");
                   this.timeOver = this.time.now;
+                },
+                spawnWall: function(y, flipped){
+                  var wall = this.walls.create(
+                    game.width,
+                    y+(flipped ? -OPENING: OPENING)/2,
+                    'wall'
+                  );
+                  this.physics.arcade.enableBody(wall);
+                  wall.body.allowGravity = false;
+                  wall.scored = false;
+                  wall.scored = false;
+                  wall.body.immovable = true;
+                  wall.body.velocity.x = -SPEED;
+                  if(flipped){
+                    wall.scale.y = -1;
+                    wall.body.offset.y = -wall.body.height;
+                  }
+                  return wall;
+                },
+                spawnWalls: function(){
+                  var wallY = this.rnd.integerInRange(game.height*.3, game.height*.7);
+                  var botWall = this.spawnWall(wallY);
+                  var topWall = this.spawnWall(wallY, true);
+
                 }
   }
   
@@ -112,5 +143,6 @@ window.onload = function() {
     'game',
     state
    )
+  
  
 };
